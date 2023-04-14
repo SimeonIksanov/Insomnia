a96145fbc7aed5a6ca6c6f887bf8f5083d5d3331
Simeon Iksanov <simeon.iksanov@gmail.com>
2023-04-14 12:16:02 +0500

    first comit

---
+++ App/PowerHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class PowerHelper
+    {
+        public static void ForceSystemAwake()
+        {
+            NativeMethods.SetThreadExecutionState(
+                NativeMethods.EXECUTION_STATE.ES_AWAYMODE_REQUIRED |
+                NativeMethods.EXECUTION_STATE.ES_CONTINUOUS |
+                NativeMethods.EXECUTION_STATE.ES_DISPLAY_REQUIRED |
+                NativeMethods.EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+        }
+
+        public static void ResetSystemDefault()
+        {
+            NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS);
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int pause = GetTimeout(args); // milliseconds
+            while (true)
+            {
+                PowerHelper.ForceSystemAwake();
+                await Task.Delay(pause);
+            }
+        }
+
+        private static int GetTimeout(string[] args)
+        {
+            if (args.Length == 1 && int.TryParse(args[0],out int pause))
+            {
+                return pause;
+            }
+
+            return 60_000;
+        }
+    }
+}
